5b7c0bf39f4836f5a01d358ba400062816661916
Aqib-dev05 <aqibgithub@gmail.com>
2026-03-31 08:19:03 +0500

    nbh

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 const jobRoutes = require('./routes/job');
